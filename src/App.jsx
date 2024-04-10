b46ee1eeb94d7512d5cd1a6bcd9960f9c0f4f76a
haroon <muhammadharoon9802@gmail.com>
2024-04-10 23:25:52 +0500

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { } from 'react'
-
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+
+      </Routes>
       <div className="w-full bg-gradient-to-r  from-neutral-900 to-slate-800">
         <h1 className="text-5xl text-white font-primary text-green">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi perferendis sit quisquam, doloribus in illo magnam adipisci dolor nulla eligendi ratione repudiandae nihil dolorum repellendus voluptatum ad nemo ex at.</h1>
         <h1 className="text-5xl  text-theme2 font-secondry ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi perferendis sit quisquam, doloribus in illo magnam adipisci dolor nulla eligendi ratione repudiandae nihil dolorum repellendus voluptatum ad nemo ex at.</h1>
